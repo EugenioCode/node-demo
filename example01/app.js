4565772267d4cc4e2b31ff33155eafa30566e3b4
eugenio <eugeniocode@yeah.net>
2022-03-09 22:47:01 +0800

    feat: 新增第一个练习demo

--- example01/app.js
+++ example01/app.js
@@ -1,19 +1,42 @@
 const http = require('http');
 const fs = require('fs');
+const queryString = require('querystring')
 
 http.createServer((req, res) => {
     let message = ''
     if(req.url === '/') {
         message = 'this is homepage'
+        res.writeHead(200, {
+            'Content-Type': 'text/html;charset=UTF8'
+        })
+        res.end(message)
     } else if(req.url === '/login') {
         message = fs.readFileSync('./template/login.html').toString()
+        res.writeHead(200, {
+            'Content-Type': 'text/html;charset=UTF8'
+        })
+        res.end(message)
     } else if(req.url === '/account') {
-        console.log(req)
+        let body = ''
+        req.on('data', (chunk) => {
+            body += chunk
+        })
+        req.on('end', () => {
+            body = queryString.parse(body)
+            if(body.username && body.pwd) {
+                res.writeHead(302, {
+                    'Location': '/'
+                })
+            } else {
+                res.writeHead(302, {
+                    'Location': '/login'
+                })
+            }
+            res.end()
+
+        })
     }
-    res.writeHead(200, {
-        'Content-Type': 'text/html;charset=UTF8'
-    })
-    res.end(message)
+
 }).listen(3000, () => {
     console.log('serve is running on port 3000')
 })
